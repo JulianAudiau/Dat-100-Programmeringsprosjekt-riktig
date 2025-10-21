a361954a94313e02d2c9c379c5082f4eef6c20eb
gronningencode <vebjorn.gronningen@gmail.com>
2025-10-21 13:27:33 +0200

    oppgave 2

--- src/main/java/no/hvl/dat100/javel/oppgave2/MonthMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave2/MonthMain.java
@@ -2,14 +2,11 @@ package no.hvl.dat100.javel.oppgave2;
 
 import no.hvl.dat100.javel.oppgave1.DayPowerData;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MonthMain {
     public static void main(String[] args) {
 
         // test data
         double[][] power_prices_month = MonthPowerData.powerprices_month;
-
         double[][] power_usage_month = MonthPowerData.powerusage_month;
 
         System.out.println("==============");
@@ -17,13 +14,45 @@ public class MonthMain {
         System.out.println("==============");
         System.out.println();
 
-        /*
-        TODO
+        // --- a) Test print_PowerUsage ---
+        System.out.println("=== Test a) print_PowerUsage ===");
+        MonthlyPower.print_PowerUsage(power_usage_month);
+        System.out.println();
 
-         Write code that tests the methods you implement in the MonthlyPower class
-         Remember to teste the methods as you implement them
-         Remember to also to check that you get the expected results
-         */
+        // --- b) Test print_PowerPrices ---
+        System.out.println("=== Test b) print_PowerPrices ===");
+        MonthlyPower.print_PowerPrices(power_prices_month);
+        System.out.println();
 
+        // --- c) Test computePowerUsage ---
+        System.out.println("=== Test c) computePowerUsage ===");
+        double totalUsage = MonthlyPower.computePowerUsage(power_usage_month);
+        System.out.printf("Total power usage for the month: %.2f kWh%n", totalUsage);
+        System.out.println();
+
+        // --- d) Test exceedThreshold ---
+        System.out.println("=== Test d) exceedThreshold ===");
+        double threshold = 120.0; // Juster etter realistisk nivå
+        boolean exceeded = MonthlyPower.exceedThreshold(power_usage_month, threshold);
+        System.out.printf("Threshold of %.1f kWh exceeded: %b%n", threshold, exceeded);
+        System.out.println();
+
+        // --- e) Test computeSpotPrice ---
+        System.out.println("=== Test e) computeSpotPrice ===");
+        double totalSpotPrice = MonthlyPower.computeSpotPrice(power_usage_month, power_prices_month);
+        System.out.printf("Total spot price for the month: %.2f NOK%n", totalSpotPrice);
+        System.out.println();
+
+        // --- f) Test computePowerSupport ---
+        System.out.println("=== Test f) computePowerSupport ===");
+        double support = MonthlyPower.computePowerSupport(power_usage_month, power_prices_month);
+        System.out.printf("Total power support for the month: %.2f NOK%n", support);
+        System.out.println();
+
+        // --- g) Test computeNorgesPrice ---
+        System.out.println("=== Test g) computeNorgesPrice ===");
+        double norgesPrice = MonthlyPower.computeNorgesPrice(power_usage_month);
+        System.out.printf("Norgespris (average hourly consumption): %.3f kWh%n", norgesPrice);
+        System.out.println();
     }
 }
--- src/main/java/no/hvl/dat100/javel/oppgave2/MonthlyPower.java
+++ src/main/java/no/hvl/dat100/javel/oppgave2/MonthlyPower.java
@@ -3,69 +3,108 @@ package no.hvl.dat100.javel.oppgave2;
 import no.hvl.dat100.javel.oppgave1.DailyPower;
 
 public class MonthlyPower {
-
-    // a) print power usage for a month
+//a
     public static void print_PowerUsage(double[][] usage) {
+        for (int day = 0; day < usage.length; day++) {
+            System.out.print("Day " + (day + 1) + ":");
 
-        // TODO
+            for (int hour = 0; hour < usage[day].length; hour++) {
+                System.out.printf("%.2f kWh ", usage[day][hour]);
+            }
 
+            System.out.println();
+        }
     }
-
-    // b) print power prices for a month
+//b
     public static void print_PowerPrices(double[][] prices) {
+        for (int day = 0; day < prices.length; day++) {
+            System.out.print("Day " + (day + 1) + ":");
 
-        // TODO
+            for (int hour = 0; hour < prices[day].length; hour++) {
+                System.out.printf("%.2f NOK ", prices[day][hour]);
+            }
 
+            System.out.println();
+        }
     }
-
-    // c) compute total power usage for a month
+    //c
     public static double computePowerUsage(double[][] usage) {
-
         double sum = 0;
 
-        // TODO
+        for (int day = 0; day < usage.length; day++) {
+            for (int hour = 0; hour < usage[day].length; hour++) {
+                sum += usage[day][hour];
+            }
+        }
 
         return sum;
     }
-
-    // d) determine whether a given threshold in powerusage for the month has been exceeded
+//d
     public static boolean exceedThreshold(double[][] powerusage, double threshold) {
 
-        boolean exceeded = false;
         double usage = 0;
 
-        // TODO
+        for (int day = 0; day < powerusage.length; day++) {
+            for (int hour = 0; hour < powerusage[day].length; hour++) {
+                usage += powerusage[day][hour];
+            }
+        }
+
+        boolean exceeded = usage > threshold;
 
         return exceeded;
     }
 
-    // e) compute spot price
+    // e
     public static double computeSpotPrice(double[][] usage, double[][] prices) {
 
         double price = 0;
 
-        // TODO
+        for (int day = 0; day < usage.length; day++) {
+            for (int hour = 0; hour < usage[day].length; hour++) {
+                price += usage[day][hour] * prices[day][hour];
+            }
+        }
 
         return price;
     }
 
-    // f) power support for the month
+    // f
     public static double computePowerSupport(double[][] usage, double[][] prices) {
 
         double support = 0;
+        double supportThreshold = 0.70;
+        double supportRate = 0.90;
 
-        // TODO
+        for (int day = 0; day < usage.length; day++) {
+            for (int hour = 0; hour < usage[day].length; hour++) {
+                double price = prices[day][hour];
+
+                if (price > supportThreshold) {
+                    double diff = price - supportThreshold;
+                    double hourlySupport = diff * supportRate * usage[day][hour];
+                    support += hourlySupport;
+                }
+            }
+        }
 
         return support;
     }
 
-    // g) Norgesprice for the month
+    // g
     public static double computeNorgesPrice(double[][] usage) {
 
-        double price = 0;
+        double sum = 0;
+        int count = 0;
 
-        // TODO
+        for (int day = 0; day < usage.length; day++) {
+            for (int hour = 0; hour < usage[day].length; hour++) {
+                sum += usage[day][hour];
+                count++;
+            }
+        }
+
+        double price = sum / count;
 
         return price;
     }
-}
