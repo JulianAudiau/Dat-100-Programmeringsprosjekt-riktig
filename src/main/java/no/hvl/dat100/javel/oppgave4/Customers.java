ba890b58d088b68818e5aad93451940d0b94c1e6
gronningencode <vebjorn.gronningen@gmail.com>
2025-10-22 11:44:48 +0200

    Oppgave 4
    Jeg har gjort og testet oppgave 4 customers

--- src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
+++ src/main/java/no/hvl/dat100/javel/oppgave4/Customers.java
@@ -8,61 +8,56 @@ public class Customers {
 
     // a) Complete constructor
     public Customers(int size) {
-
-        // TODO
-
+        // Initialiserer arrayen med ønsket størrelse
+        customers = new Customer[size];
     }
 
     // b) count number of non-null references
     public int countNonNull() {
-
-
         int count = 0;
-
-        // TODO
-
+        for (Customer c : customers) {
+            if (c != null) {
+                count++;
+            }
+        }
         return count;
     }
 
     // c) return reference to customer with given id (if exists)
     public Customer getCustomer(int customer_id) {
-
-        boolean funnet = false;
-        Customer c = null;
-
-        // TODO
-
-        return c;
+        for (Customer c : customers) {
+            if (c != null && c.getid() == customer_id) { // forutsetter at Customer har getId()
+                return c;
+            }
+        }
+        return null;
     }
 
     // d) add a customer to the reference table
     public boolean addCustomer(Customer c) {
-
-        boolean inserted = false;
-
-        // TODO
-
-        return inserted;
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i] == null) {
+                customers[i] = c;
+                return true;
+            }
+        }
+        return false; // ingen ledig plass
     }
 
     // e) remove customer with given id from reference table
     public Customer removeCustomer(int customer_id) {
-
-        boolean deleted = false;
-        Customer c = null;
-
-        // TODO
-
-        return c;
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i] != null && customers[i].getId() == customer_id) {
+                Customer removed = customers[i];
+                customers[i] = null; // fjerner referansen
+                return removed;
+            }
+        }
+        return null; // finnes ikke
     }
 
     // f) return reference table with all customers
     public Customer[] getCustomers() {
-
-        Customer[] customers = null;
-
-        // TODO
-
         return customers;
     }
 }
--- src/main/java/no/hvl/dat100/javel/oppgave4/CustomersMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave4/CustomersMain.java
@@ -12,12 +12,49 @@ public class CustomersMain {
         System.out.println("==============");
         System.out.println();
 
-        /*
-        TODO
+        Customers customers = new Customers(5);
 
-         Write code that uses and tests the methods implemented in the Customers class
+        Customer c1 = new Customer(1, "Ola Nordmann", PowerAgreementType.STANDARD);
+        Customer c2 = new Customer(2, "Kari Nordmann", PowerAgreementType.GREEN);
+        Customer c3 = new Customer(3, "Per Hansen", PowerAgreementType.STANDARD);
 
-        */
+        // d)
+        System.out.println("Legger til c1: " + customers.addCustomer(c1)); // true
+        System.out.println("Legger til c2: " + customers.addCustomer(c2)); // true
+        System.out.println("Legger til c3: " + customers.addCustomer(c3)); // true
 
+        // b)
+        System.out.println("Antall kunder: " + customers.countNonNull()); // 3
+
+        // c)
+        Customer hentet = customers.getCustomer(2);
+        System.out.println("Hentet kunde med id 2: " + (hentet != null ? hentet.getName() : "Ikke funnet"));
+
+        // e)
+        Customer fjernet = customers.removeCustomer(1);
+        System.out.println("Fjernet kunde: " + (fjernet != null ? fjernet.getName() : "Ikke funnet"));
+
+        System.out.println("Antall kunder etter fjerning: " + customers.countNonNull());
+
+        // f)
+        System.out.println("Alle kunder i tabellen:");
+        for (Customer c : customers.getCustomers()) {
+            if (c != null) {
+                System.out.println(c.getId() + ": " + c.getName() + " (" + c.getAgreement() + ")");
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        // teste antall kunder
+        Customer c4 = new Customer(4, "Anne Olsen", PowerAgreementType.GREEN);
+        Customer c5 = new Customer(5, "Jonas Berg", PowerAgreementType.STANDARD);
+        Customer c6 = new Customer(6, "Lise Nilsen", PowerAgreementType.STANDARD);
+
+        System.out.println("Legger til c4: " + customers.addCustomer(c4)); // true
+        System.out.println("Legger til c5: " + customers.addCustomer(c5)); // true
+        System.out.println("Legger til c6 (for mye): " + customers.addCustomer(c6)); // false
+
+        System.out.println("Antall kunder til slutt: " + customers.countNonNull());
     }
 }
